92c14acd11ddab8f02913503dd900d74f3cfcbc5
cjv012 <76981927+cjv012@users.noreply.github.com>
2023-09-01 13:57:20 +0000

    Pitch Averages Complete

--- pitcher.py
+++ pitcher.py
@@ -35,14 +35,14 @@ class Pitcher:
         totSpin = 0
         totStrike = 0
         for pitch in self.pitches:
-            if pitch.ptype == "Fastball":
+            if pitch.ptype == "ChangeUp":
                 numPitches += 1
                 totVelo += float(pitch.velocity)
                 totSpin += float(pitch.spin)
                 if pitch.outcome == "StrikeCalled":
                     totStrike += 1
         if (numPitches > 0): 
-            return("Fastballs Thrown: " + str(numPitches) + "\n" + "Average Fastball Velocity: " + str(round((totVelo/numPitches), 1)) + "\n" + "Average Fastball Spin Rate: " + str(round((totSpin/numPitches), 1)) + "\n" + "Fastball Strike Percentage: " + str(round(((totStrike/numPitches) * 100), 1)) + "%")
+            return("Changeups Thrown: " + str(numPitches) + "\n" + "Average Changeup Velocity: " + str(round((totVelo/numPitches), 1)) + "\n" + "Average Changeup Spin Rate: " + str(round((totSpin/numPitches), 1)) + "\n" + "Changeup Strike Percentage: " + str(round(((totStrike/numPitches) * 100), 1)) + "%")
         else:
             return ""
         
@@ -52,14 +52,14 @@ class Pitcher:
         totSpin = 0
         totStrike = 0
         for pitch in self.pitches:
-            if pitch.ptype == "Fastball":
+            if pitch.ptype == "Curveball":
                 numPitches += 1
                 totVelo += float(pitch.velocity)
                 totSpin += float(pitch.spin)
                 if pitch.outcome == "StrikeCalled":
                     totStrike += 1
         if (numPitches > 0): 
-            return("Fastballs Thrown: " + str(numPitches) + "\n" + "Average Fastball Velocity: " + str(round((totVelo/numPitches), 1)) + "\n" + "Average Fastball Spin Rate: " + str(round((totSpin/numPitches), 1)) + "\n" + "Fastball Strike Percentage: " + str(round(((totStrike/numPitches) * 100), 1)) + "%")
+            return("Curveballs Thrown: " + str(numPitches) + "\n" + "Average Curveball Velocity: " + str(round((totVelo/numPitches), 1)) + "\n" + "Average Curveball Spin Rate: " + str(round((totSpin/numPitches), 1)) + "\n" + "Curveball Strike Percentage: " + str(round(((totStrike/numPitches) * 100), 1)) + "%")
         else:
             return ""
         
@@ -69,14 +69,14 @@ class Pitcher:
         totSpin = 0
         totStrike = 0
         for pitch in self.pitches:
-            if pitch.ptype == "Fastball":
+            if pitch.ptype == "Slider":
                 numPitches += 1
                 totVelo += float(pitch.velocity)
                 totSpin += float(pitch.spin)
                 if pitch.outcome == "StrikeCalled":
                     totStrike += 1
         if (numPitches > 0): 
-            return("Fastballs Thrown: " + str(numPitches) + "\n" + "Average Fastball Velocity: " + str(round((totVelo/numPitches), 1)) + "\n" + "Average Fastball Spin Rate: " + str(round((totSpin/numPitches), 1)) + "\n" + "Fastball Strike Percentage: " + str(round(((totStrike/numPitches) * 100), 1)) + "%")
+            return("Sliders Thrown: " + str(numPitches) + "\n" + "Average Slider Velocity: " + str(round((totVelo/numPitches), 1)) + "\n" + "Average Slider Spin Rate: " + str(round((totSpin/numPitches), 1)) + "\n" + "Slider Strike Percentage: " + str(round(((totStrike/numPitches) * 100), 1)) + "%")
         else:
             return ""
         
@@ -86,14 +86,14 @@ class Pitcher:
         totSpin = 0
         totStrike = 0
         for pitch in self.pitches:
-            if pitch.ptype == "Fastball":
+            if pitch.ptype == "Splitter":
                 numPitches += 1
                 totVelo += float(pitch.velocity)
                 totSpin += float(pitch.spin)
                 if pitch.outcome == "StrikeCalled":
                     totStrike += 1
         if (numPitches > 0): 
-            return("Fastballs Thrown: " + str(numPitches) + "\n" + "Average Fastball Velocity: " + str(round((totVelo/numPitches), 1)) + "\n" + "Average Fastball Spin Rate: " + str(round((totSpin/numPitches), 1)) + "\n" + "Fastball Strike Percentage: " + str(round(((totStrike/numPitches) * 100), 1)) + "%")
+            return("Splitters Thrown: " + str(numPitches) + "\n" + "Average Splitter Velocity: " + str(round((totVelo/numPitches), 1)) + "\n" + "Average Splitter Spin Rate: " + str(round((totSpin/numPitches), 1)) + "\n" + "Splitter Strike Percentage: " + str(round(((totStrike/numPitches) * 100), 1)) + "%")
         else:
             return ""
         
@@ -103,14 +103,14 @@ class Pitcher:
         totSpin = 0
         totStrike = 0
         for pitch in self.pitches:
-            if pitch.ptype == "Fastball":
+            if pitch.ptype == "Cutter":
                 numPitches += 1
                 totVelo += float(pitch.velocity)
                 totSpin += float(pitch.spin)
                 if pitch.outcome == "StrikeCalled":
                     totStrike += 1
         if (numPitches > 0): 
-            return("Fastballs Thrown: " + str(numPitches) + "\n" + "Average Fastball Velocity: " + str(round((totVelo/numPitches), 1)) + "\n" + "Average Fastball Spin Rate: " + str(round((totSpin/numPitches), 1)) + "\n" + "Fastball Strike Percentage: " + str(round(((totStrike/numPitches) * 100), 1)) + "%")
+            return("Cutters Thrown: " + str(numPitches) + "\n" + "Average Cutter Velocity: " + str(round((totVelo/numPitches), 1)) + "\n" + "Average Cutter Spin Rate: " + str(round((totSpin/numPitches), 1)) + "\n" + "Cutter Strike Percentage: " + str(round(((totStrike/numPitches) * 100), 1)) + "%")
         else:
             return ""
         
@@ -120,14 +120,14 @@ class Pitcher:
         totSpin = 0
         totStrike = 0
         for pitch in self.pitches:
-            if pitch.ptype == "Fastball":
+            if pitch.ptype == "Sinker":
                 numPitches += 1
                 totVelo += float(pitch.velocity)
                 totSpin += float(pitch.spin)
                 if pitch.outcome == "StrikeCalled":
                     totStrike += 1
         if (numPitches > 0): 
-            return("Fastballs Thrown: " + str(numPitches) + "\n" + "Average Fastball Velocity: " + str(round((totVelo/numPitches), 1)) + "\n" + "Average Fastball Spin Rate: " + str(round((totSpin/numPitches), 1)) + "\n" + "Fastball Strike Percentage: " + str(round(((totStrike/numPitches) * 100), 1)) + "%")
+            return("Sinkers Thrown: " + str(numPitches) + "\n" + "Average Sinker Velocity: " + str(round((totVelo/numPitches), 1)) + "\n" + "Average Sinker Spin Rate: " + str(round((totSpin/numPitches), 1)) + "\n" + "Sinker Strike Percentage: " + str(round(((totStrike/numPitches) * 100), 1)) + "%")
         else:
             return ""
         
@@ -137,13 +137,13 @@ class Pitcher:
         totSpin = 0
         totStrike = 0
         for pitch in self.pitches:
-            if pitch.ptype == "Fastball":
+            if pitch.ptype == "TwoSeamFastBall":
                 numPitches += 1
                 totVelo += float(pitch.velocity)
                 totSpin += float(pitch.spin)
                 if pitch.outcome == "StrikeCalled":
                     totStrike += 1
         if (numPitches > 0): 
-            return("Fastballs Thrown: " + str(numPitches) + "\n" + "Average Fastball Velocity: " + str(round((totVelo/numPitches), 1)) + "\n" + "Average Fastball Spin Rate: " + str(round((totSpin/numPitches), 1)) + "\n" + "Fastball Strike Percentage: " + str(round(((totStrike/numPitches) * 100), 1)) + "%")
+            return("TwoSeams Thrown: " + str(numPitches) + "\n" + "Average TwoSeam Velocity: " + str(round((totVelo/numPitches), 1)) + "\n" + "Average TwoSeam Spin Rate: " + str(round((totSpin/numPitches), 1)) + "\n" + "Fastball TwoSeam Percentage: " + str(round(((totStrike/numPitches) * 100), 1)) + "%")
         else:
             return ""
