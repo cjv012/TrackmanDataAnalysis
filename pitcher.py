3aab9f30190f8d653b8c6607d175d9181091829a
cjv012 <76981927+cjv012@users.noreply.github.com>
2023-09-12 16:31:15 +0000

    Updated Strike Percentage

--- pitcher.py
+++ pitcher.py
@@ -35,7 +35,7 @@ class Pitcher:
                 totSpin += float(pitch.spin)
                 totVBreak += float(pitch.vbreak)
                 totHBreak += float(pitch.hbreak)
-                if pitch.outcome == "StrikeCalled":
+                if pitch.outcome in ["StrikeCalled", "InPlay", "StrikeSwinging", "FoulBall"]:
                     totStrike += 1
         if (numPitches > 0): 
             return("Fastballs Thrown: " + str(numPitches) + "\n" + "Average Fastball Velocity: " + str(round((totVelo/numPitches), 1)) + "\n" + "Max Fastball Velocity: " + str(round((maxFast), 1)) + "\n" + "Average Fastball Spin Rate: " + str(round((totSpin/numPitches), 1)) + "\n" + "Average Fastball Vertical Break: " + str(round((totVBreak/numPitches), 1)) + "\n" + "Average Fastball Horizontal Break: " + str(round((totHBreak/numPitches), 1)) + "\n" "Fastball Strike Percentage: " + str(round(((totStrike/numPitches) * 100), 1)) + "%")
@@ -57,7 +57,7 @@ class Pitcher:
                 totSpin += float(pitch.spin)
                 totVBreak += float(pitch.vbreak)
                 totHBreak += float(pitch.hbreak)
-                if pitch.outcome == "StrikeCalled":
+                if pitch.outcome in ["StrikeCalled", "InPlay", "StrikeSwinging", "FoulBall"]:
                     totStrike += 1
         if (numPitches > 0): 
             return("Changeups Thrown: " + str(numPitches) + "\n" + "Average Changeup Velocity: " + str(round((totVelo/numPitches), 1)) + "\n" + "Average Changeup Spin Rate: " + str(round((totSpin/numPitches), 1)) + "\n" + "Average Changeup Vertical Break: " + str(round((totVBreak/numPitches), 1)) + "\n" + "Average Changeup Horizontal Break: " + str(round((totHBreak/numPitches), 1)) + "\n" "Changeup Strike Percentage: " + str(round(((totStrike/numPitches) * 100), 1)) + "%")
@@ -79,7 +79,7 @@ class Pitcher:
                 totSpin += float(pitch.spin)
                 totVBreak += float(pitch.vbreak)
                 totHBreak += float(pitch.hbreak)
-                if pitch.outcome == "StrikeCalled":
+                if pitch.outcome in ["StrikeCalled", "InPlay", "StrikeSwinging", "FoulBall"]:
                     totStrike += 1
         if (numPitches > 0): 
             return("Cuverballs Thrown: " + str(numPitches) + "\n" + "Average Cuverball Velocity: " + str(round((totVelo/numPitches), 1)) + "\n" + "Average Cuverball Spin Rate: " + str(round((totSpin/numPitches), 1)) + "\n" + "Average Cuverball Vertical Break: " + str(round((totVBreak/numPitches), 1)) + "\n" + "Average Cuverball Horizontal Break: " + str(round((totHBreak/numPitches), 1)) + "\n" "Cuverball Strike Percentage: " + str(round(((totStrike/numPitches) * 100), 1)) + "%")
@@ -101,7 +101,7 @@ class Pitcher:
                 totSpin += float(pitch.spin)
                 totVBreak += float(pitch.vbreak)
                 totHBreak += float(pitch.hbreak)
-                if pitch.outcome == "StrikeCalled":
+                if pitch.outcome in ["StrikeCalled", "InPlay", "StrikeSwinging", "FoulBall"]:
                     totStrike += 1
         if (numPitches > 0): 
             return("Sliders Thrown: " + str(numPitches) + "\n" + "Average Slider Velocity: " + str(round((totVelo/numPitches), 1)) + "\n" + "Average Slider Spin Rate: " + str(round((totSpin/numPitches), 1)) + "\n" + "Average Slider Vertical Break: " + str(round((totVBreak/numPitches), 1)) + "\n" + "Average Slider Horizontal Break: " + str(round((totHBreak/numPitches), 1)) + "\n" "Slider Strike Percentage: " + str(round(((totStrike/numPitches) * 100), 1)) + "%")
@@ -123,7 +123,7 @@ class Pitcher:
                 totSpin += float(pitch.spin)
                 totVBreak += float(pitch.vbreak)
                 totHBreak += float(pitch.hbreak)
-                if pitch.outcome == "StrikeCalled":
+                if pitch.outcome in ["StrikeCalled", "InPlay", "StrikeSwinging", "FoulBall"]:
                     totStrike += 1
         if (numPitches > 0): 
             return("Splitters Thrown: " + str(numPitches) + "\n" + "Average Splitter Velocity: " + str(round((totVelo/numPitches), 1)) + "\n" + "Average Splitter Spin Rate: " + str(round((totSpin/numPitches), 1)) + "\n" + "Average Splitter Vertical Break: " + str(round((totVBreak/numPitches), 1)) + "\n" + "Average Splitter Horizontal Break: " + str(round((totHBreak/numPitches), 1)) + "\n" "Splitter Strike Percentage: " + str(round(((totStrike/numPitches) * 100), 1)) + "%")
@@ -145,7 +145,7 @@ class Pitcher:
                 totSpin += float(pitch.spin)
                 totVBreak += float(pitch.vbreak)
                 totHBreak += float(pitch.hbreak)
-                if pitch.outcome == "StrikeCalled":
+                if pitch.outcome in ["StrikeCalled", "InPlay", "StrikeSwinging", "FoulBall"]:
                     totStrike += 1
         if (numPitches > 0): 
             return("Cutters Thrown: " + str(numPitches) + "\n" + "Average Cutter Velocity: " + str(round((totVelo/numPitches), 1)) + "\n" + "Average Cutter Spin Rate: " + str(round((totSpin/numPitches), 1)) + "\n" + "Average Cutter Vertical Break: " + str(round((totVBreak/numPitches), 1)) + "\n" + "Average Cutter Horizontal Break: " + str(round((totHBreak/numPitches), 1)) + "\n" "Cutter Strike Percentage: " + str(round(((totStrike/numPitches) * 100), 1)) + "%")
@@ -167,7 +167,7 @@ class Pitcher:
                 totSpin += float(pitch.spin)
                 totVBreak += float(pitch.vbreak)
                 totHBreak += float(pitch.hbreak)
-                if pitch.outcome == "StrikeCalled":
+                if pitch.outcome in ["StrikeCalled", "InPlay", "StrikeSwinging", "FoulBall"]:
                     totStrike += 1
         if (numPitches > 0): 
             return("Sinkers Thrown: " + str(numPitches) + "\n" + "Average Sinker Velocity: " + str(round((totVelo/numPitches), 1)) + "\n" + "Average Sinker Spin Rate: " + str(round((totSpin/numPitches), 1)) + "\n" + "Average Sinker Vertical Break: " + str(round((totVBreak/numPitches), 1)) + "\n" + "Average Sinker Horizontal Break: " + str(round((totHBreak/numPitches), 1)) + "\n" "Sinker Strike Percentage: " + str(round(((totStrike/numPitches) * 100), 1)) + "%")
@@ -189,7 +189,7 @@ class Pitcher:
                 totSpin += float(pitch.spin)
                 totVBreak += float(pitch.vbreak)
                 totHBreak += float(pitch.hbreak)
-                if pitch.outcome == "StrikeCalled":
+                if pitch.outcome in ["StrikeCalled", "InPlay", "StrikeSwinging", "FoulBall"]:
                     totStrike += 1
         if (numPitches > 0):
             return("TwoSeams Thrown: " + str(numPitches) + "\n" + "Average TwoSeam Velocity: " + str(round((totVelo/numPitches), 1)) + "\n" + "Average TwoSeam Spin Rate: " + str(round((totSpin/numPitches), 1)) + "\n" + "Average TwoSeam Vertical Break: " + str(round((totVBreak/numPitches), 1)) + "\n" + "Average TwoSeam Horizontal Break: " + str(round((totHBreak/numPitches), 1)) + "\n" "TwoSeam Strike Percentage: " + str(round(((totStrike/numPitches) * 100), 1)) + "%")
